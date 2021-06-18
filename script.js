98d93f68cb05e8fbc3dd6d757220063bd21a75a8
mazambani <85748719+mazambani@users.noreply.github.com>
2021-06-18 16:54:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('alternate').addEventListener('click', () => {
+        Array.prototype.forEach.call(document.querySelectorAll('tr'), (el, i) => {
+            if (i % 2 == 0) {
+                return null;
+            }
 
+            if (el.classList.contains('bg-dark')) {
+                el.classList.remove('bg-dark')
+                el.classList.remove('text-white')
+                return null;
+            }
+
+            el.classList.add('bg-dark')
+            el.classList.add('text-white')
+        })
+    })
+})
